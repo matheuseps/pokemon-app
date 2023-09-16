db9de7e918a32ff88c85801639bc4c5d3d020f1d
matheuseps <85567598+matheuseps@users.noreply.github.com>
2023-09-15 23:08:39 -0300

    mudei

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <header className='flex justify-between max-w-5xl mx-auto my-4'>
+        <p className='lex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'> Pokemon API</p>
+        <nav className='flex gap-4'>
+      <link href='/' className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'>Home</link>
+      <link href='/Sobre' className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30' >Sobre</link>
+        </nav>
+      </header>
+      <main className='mx-auto max-w-5xl'>
+        {children}
+      </main>
+      </body>
     </html>
-  )
+  );
 }
